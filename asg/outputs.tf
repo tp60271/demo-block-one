b43341503af2da572c9a7d934b1fd25ab83a6cfe
Thiru <thiru@ntucenterprise.sg>
2020-09-08 22:44:19 +0800

    asg: updated tf variables in output

--- asg/outputs.tf
+++ asg/outputs.tf
@@ -1,15 +1,15 @@
 output "security_group" {
-  value = "${aws_security_group.bo-default.id}"
+  value = "${aws_security_group.bo-asg-default.id}"
 }
 
 output "launch_configuration" {
-  value = "${aws_launch_configuration.bo-web-lc.id}"
+  value = "${aws_launch_configuration.bo-asg-web-lc.id}"
 }
 
 output "asg_name" {
-  value = "${aws_autoscaling_group.bo-web-asg.id}"
+  value = "${aws_autoscaling_group.bo-asg-web-asg.id}"
 }
 
 output "elb_name" {
-  value = "${aws_elb.bo-web-elb.dns_name}"
+  value = "${aws_elb.bo-asg-web-elb.dns_name}"
 }
